9ab06bf336190ce94a2a9bbe771119cc6d6580d2
Pratiksha-Badakh <20095160@mydbs.ie>
2026-04-30 21:17:20 +0100

    Update ex3.js

--- ex3.js
+++ ex3.js
@@ -1,4 +1,14 @@
-let listEuler1=(a,b,l)=>0;//obviously wrong - compute the sum of multiples of a or b in list l
+let listEuler1=(a,b,l)=>{
+    let sum = 0;
+    for(let x of l)
+    {
+        if(a%x===0 || b%x===0){
+            sum+=x
+        }
+    }
+    alert("Sum of multiples of a or b in list l" + sum);
+}
+
 let listEuler2=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
 let listEuler3=(a,l)=>0;//obviously wrong - compute the sum of multiples in list a found in list l
 
@@ -7,16 +17,9 @@ let eulerlist=()=>
    let a = parseInt(document.getElementById('a').value);
    let b = parseInt(document.getElementById('b').value);
    let l = document.getElementById('l').value.split(" ").map((x=>parseInt(x));
-   let sum = 0;
-    for(let x of l)
-    {
-        if(a%x===0 || b%x===0){
-            sum+=x
-        }
-    }
-    alert("Sum of multiples of a or b in list l" + sum);
-    
-}
+   listEuler1(a,b,l);
+};
+
 let euler2Lists=()=>
 {
     a=[2,3]//can un-hardcode
